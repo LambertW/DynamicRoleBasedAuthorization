8751e4e9167d2bcc7bbc686601e6e5717136a18b
LambertWu <26795806@qq.com>
2018-06-21 23:15:10 +0800

    Init project

---
+++ DynamicRoleBasedAuthorization.OriginalWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DynamicRoleBasedAuthorization.OriginalWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
